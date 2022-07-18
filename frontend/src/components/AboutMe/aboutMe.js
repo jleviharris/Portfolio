be868f9e2e32f83daa2088a14c0816c690ccd37d
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-07-18 18:03:04 -0400

    Updated about me info

--- frontend/src/components/AboutMe/aboutMe.js
+++ frontend/src/components/AboutMe/aboutMe.js
@@ -13,9 +13,15 @@ const AboutMe = () => {
       <p className="about-me-text">
         {" "}
         I am a Software Developer currently living in the small state of
-        Delaware with my girlfriend and our two cats. Driven, hard-working, problem solver with a high
+        Delaware with my girlfriend and our two cats. After 28 years working a
+        handfull of jobs ranging from managing a Planet Fitness to Combat
+        Engingeer in the Marine Corps, I stumbled upon my true passion of
+        Software Development. Driven, hard-working, problem solver with a high
         attention to detail. Fan of anime, motorcycles, video games, and almost
-        any outdoor activity.
+        any outdoor activity. Currently working with the MERN stack. Proficient
+        with HTML, CSS, JavaScript, MongoDB, Express, React, Node.js, Postman,
+        Git, and Github. Interested in expanding my knowledge and progressing my
+        career as a software engineer.
       </p>
     </div>
   );
