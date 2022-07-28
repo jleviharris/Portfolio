fcb915dff0b92b71e74fdbdd25295cda4212ec57
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-07-28 18:23:50 -0400

    Final commit for the night / saving progress

--- frontend/src/components/Projects/project3.js
+++ frontend/src/components/Projects/project3.js
@@ -2,7 +2,7 @@ const Project3 = () => {
     return (
       <div className="projectContainer">
         <h1>Portfolio Website</h1>
-        <p>First portfolio website that I built using HTML, CSS, JavaScript and Github pages</p>
+        <p>Mobile and desktop responsive portfolio website built using HTML, CSS, JavaScript and Github pages</p>
         <img className="projectImg" src={require("./imgProjects/OriginalPortfolio.png")} />
       </div>
     );
