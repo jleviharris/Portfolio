49b943f2eb163c9e5add76a298361ddc8ab5cde4
Jonathan Harris <jleviharris@aol.com>
2024-04-11 01:48:11 -0400

    Update 2024

--- frontend/src/components/AboutMe/aboutMe.js
+++ frontend/src/components/AboutMe/aboutMe.js
@@ -17,32 +17,25 @@ const AboutMe = () => {
       </div>
       <div className="about-me-text">
         <div className="about-me-para">
-          I'm Jonathan, a full stack web developer working in the MERN stack. My
-          experiance ranges from Combat Engineer in the Marine Corps to GM of a
-          franchise Planet Fitness and now to Software Developer. My coding
-          journey began in late 2021 with the discovery of freeCodeCamp and
-          codecademy. By Feb 2022 I was enrolled with the coding bootcamp
-          devCodeCamp and I had graduted by the end of May. Now I am searching
-          for a position where I can further grow and learn as a developer.{" "}
+       With over four years of experience in the fitness industry, four years as a Combat Engineer in the Marine Corps and a recent career transition into software development, I am a versatile and adaptable professional who can thrive in different environments and challenges. I have a passion for creating user-friendly and responsive web applications that solve real-world problems and enhance customer satisfaction. I graduated from devCodeCamp, a rigorous and immersive coding bootcamp, where I learned and applied various technologies.
+
+<br/><br/>Currently, I am a software developer at Horizon Systems & Services, a leading provider of IT solutions and services for various clients in the Property and Casualty Insurance Industry. In this role, I work with a team of talented and experienced developers to design, develop, test, and deploy applications that meet the specifications and requirements of our customers.{" "}
         </div>
         <div className="allIcons">
           <div className="iconsRow">
             <div className="singleTech">
-              <i className="fa-brands fa-html5 icons"></i>HTML
+              <iconify-icon icon="skill-icons:html"></iconify-icon>HTML
             </div>
             <div className="singleTech">
-              <i className="fa-brands fa-css3-alt icons"></i>CSS
+              <iconify-icon icon="skill-icons:css"></iconify-icon>CSS
             </div>{" "}
             <div className="singleTech">
-              <i className="fa-brands fa-square-js icons"></i>JavaScript
+              <iconify-icon icon="skill-icons:javascript"></iconify-icon>JavaScript
             </div>
           </div>
           <div className="iconsRow">
             <div className="singleTech">
-              <iconify-icon
-                icon="simple-icons:mongodb"
-                class="icons"
-              ></iconify-icon>
+         <iconify-icon icon="skill-icons:mongodb"></iconify-icon>
               MongoDB
             </div>
             <div className="middleTech">
@@ -56,16 +49,46 @@ const AboutMe = () => {
               ></iconify-icon>
               Express.js
             </div>
+               <div className="singleTech">
+            <iconify-icon icon="fluent:code-vb-16-filled"></iconify-icon>
+              Visual Basic
+            </div>
+                <div className="singleTech">
+            <iconify-icon icon="skill-icons:angular-dark"></iconify-icon>
+              Angular
+            </div>
+                 <div className="singleTech">
+            <iconify-icon icon="skill-icons:cs"></iconify-icon>
+              C#
+            </div>
+                    <div className="singleTech">
+            <iconify-icon icon="skill-icons:typescript"></iconify-icon>
+              TypeScript
+            </div>
+                      <div className="singleTech">
+              <iconify-icon icon="skill-icons:dotnet"></iconify-icon>
+              .NET
+            </div>
+                       <div className="singleTech">
+               <iconify-icon icon="devicon:azuredevops"></iconify-icon>
+              Azure DevOps
+            </div>
+
+                          <div className="singleTech">
+                    <iconify-icon icon="devicon:microsoftsqlserver"></iconify-icon>
+              Microsoft SQL Server
+            </div>
+    
           </div>
           <div className="iconsRow">
             <div className="singleTech">
-              <i className="fa-brands fa-react icons"></i>React.js
+              <iconify-icon icon="skill-icons:react-dark"></iconify-icon>React.js
             </div>
             <div className="singleTech">
-              <i className="fa-brands fa-node icons"></i>Node.js
+              <iconify-icon icon="skill-icons:nodejs-dark"></iconify-icon>Node.js
             </div>
             <div className="singleTech">
-              <i className="fa-brands fa-github icons"></i> Github
+              <iconify-icon icon="skill-icons:git"></iconify-icon> Git
             </div>
           </div>
         </div>
